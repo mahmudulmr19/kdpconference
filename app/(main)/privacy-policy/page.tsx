d94515228189624b1196c44a706688d2f2742dbc
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-09-07 01:09:10 +0600

    privacy policy and refund policy page added

---
+++ app/(main)/privacy-policy/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return <section className="container_section">PrivacyPolicy</section>;
+}
---
+++ app/(main)/refund-policy/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RefundPolicy() {
+  return <section className="container_section">RefundPolicy</section>;
+}
