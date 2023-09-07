912aa82e2b22f572e178a54c2294e51ad157e2fe
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-09-07 23:57:06 +0600

    updated privacy policy

--- app/(main)/privacy-policy/page.tsx
+++ app/(main)/privacy-policy/page.tsx
@@ -1,5 +1,63 @@
 import React from "react";
 
 export default function PrivacyPolicy() {
-  return <section className="container_section">PrivacyPolicy</section>;
+  return (
+    <section className="container_section py-[5rem!important]">
+      <h1 className="text-2xl font-bold text-gray-900">Privacy Policy</h1>
+
+      <div className="space-y-4 mt-4 text-gray-800">
+        <p>
+          By Purchasing Ticket on our website, you agree that you have carefully
+          read, acknowledged, understood, and provided consent to the following
+          “Privacy Policy” of{" "}
+          <a href="https://www.kdpconference.com" className="text-orange-500">
+            https://www.kdpconference.com
+          </a>
+          . So, please read our privacy and policy very carefully!!
+        </p>
+
+        <p>
+          We respect your privacy and are dedicated to protecting the privacy of
+          those persons (“You”) participating in events organized by Learn With
+          Rakib and using the Events website and associated services
+          (“Services”). Information can be shared for promotional purposes. This
+          Privacy Policy helps You to understand what Personal Information (as
+          defined below) and non-personal information We collect, and how We use
+          it. We are deeply committed to the following morals of our Privacy &
+          Policy:
+        </p>
+
+        <ul className="space-y-2">
+          <li>
+            • You will give accurate information about yourself and be aware of
+            our policy
+          </li>
+          <li>• We are keeping the privacy of our respected users.</li>
+          <li>• Our aim is to connect authors in Bangladesh in one place.</li>
+          <li>
+            • A cookie is a string of information or a small text file that a
+            website stores on a visitor’s device, and that the visitor’s browser
+            or operating system provides to “remember” things about your visit.
+            Now we are not using cookies for tracking purposes.
+          </li>
+          <li>
+            • Third-party disclosure: We strictly maintain the policy of not
+            selling, dealing, or transferring your personal information to third
+            parties. Not even use content, services, or information from
+            third-party sites on our platform.
+          </li>
+
+          <li>
+            • You are our valued audience who wants to join us for a better
+            future. As we are concerned and respect your emotions and
+            confidentiality, you also must not do the following with our
+            website: - Using our site for any illegal purposes. - Infringe the
+            Copyrights. - Harassing or personally attacking other users.
+          </li>
+        </ul>
+
+        <p>You may at any time contact Management Events through contact.</p>
+      </div>
+    </section>
+  );
 }
