892414321fa850e82aad31ff9a496e624edf68b8
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-09-07 21:57:00 +0600

    updated button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-orange-500 hover:bg-orange-500/90 text-white focus-visible:ring-orange-500 hover:focus-visible:ring-orange-500/90",
+          "bg-orange-500 hover:bg-orange-500/90 text-white focus:ring-orange-500 hover:focus:ring-orange-500/90",
       },
       size: {
         default: "h-10 px-4 py-2",
