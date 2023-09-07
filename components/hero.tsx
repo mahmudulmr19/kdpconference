075fbc0415770b38fb194c464f005855b1fe935a
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-09-07 15:12:48 +0600

    updated hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export function Hero() {
   return (
     <header role="banner" className="relative min-h-[92vh]">
-      <div className="bg-[url('/hero.jpg')] absolute top-0 bottom-0 left-0 right-0 bg-cover h-full">
+      <div className="bg-[url('/hero.jpg')] absolute top-0 bottom-0 left-0 right-0 bg-cover bg-fixed h-full">
         <div className="absolute top-0 bottom-0 left-0 right-0 bg-cover bg-black/50" />
 
         <div className="container_section text-center relative z-10 h-full flex flex-col justify-center gap-4">
