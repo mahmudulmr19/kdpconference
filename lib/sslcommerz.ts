f022615597324203244162a0efbb678d6cc17de6
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-09-08 20:47:18 +0600

    modified:   lib/sslcommerz.ts

--- lib/sslcommerz.ts
+++ lib/sslcommerz.ts
@@ -2,6 +2,6 @@ import { SslCommerzPayment } from "sslcommerz";
 
 const storeId = process.env.SSL_STORE_ID;
 const storePassword = process.env.SSL_SECRET_KEY;
-const live = true;
+const live = false;
 
 export const sslcommerz = new SslCommerzPayment(storeId, storePassword, live);
