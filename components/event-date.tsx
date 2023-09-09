d0150d0223d5b06f1bb11589481aa9dc9effa3ee
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-09-10 01:50:46 +0600

    modified:   components/event-date.tsx

--- components/event-date.tsx
+++ components/event-date.tsx
@@ -4,7 +4,7 @@ export function EventDate() {
   return (
     <section className="bg-black mb-10">
       <div className="container_section flex items-center justify-center">
-        <h2 className="text-2xl sm:text-3xl max-w-screen-md font-bold text-white text-center">
+        <h2 className="text-2xl max-w-screen-md font-bold text-white text-center">
           Friday, 20th October, 2023 | Western Cuisine Restaurant and Convention
           Center
         </h2>
